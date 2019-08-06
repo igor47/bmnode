6e1f5dfdc11c3349622ef88145ffd162203c57bb
Claire Nord <cnord@mit.edu>
2019-08-06 20:25:39 +0000

    log_config: use custom formatter

--- log_config.py
+++ log_config.py
@@ -1,8 +1,13 @@
 import logging
 
+class BMNodeFormatter(logging.Formatter):
+    def formatMessage(self, record):
+        prefix = "%(levelname)s %(name)s" % record.__dict__
+        return f"{prefix} : {record.message}"
 
 def log_config(level: int = logging.DEBUG) -> None:
     stream = logging.StreamHandler()
+    stream.setFormatter(BMNodeFormatter())
 
     root = logging.getLogger()
     root.addHandler(stream)
